41cd4e9dc4784650029f0bd64b88d3866bb2e304
dongnb <dongnb@tima.vn>
2025-02-14 14:11:27 +0700

    demo n8n

---
+++ test.py
@@ -0,0 +1,11 @@
+Đoạn text bạn cung cấp là: "loa".
+
+Chuyển đổi sang Input Binary Field (dạng mã nhị phân), đoạn văn bản sẽ được biểu diễn như sau:
+
+- "l" = 01101100 
+- "o" = 01101111 
+- "a" = 01100001 
+
+Ghép lại, "loa" trong mã nhị phân là: **01101100 01101111 01100001**.
+
+Nếu bạn cần gì thêm, hãy cho tôi biết!
